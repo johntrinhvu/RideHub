9491395d82a40628055f0f887bfecf2b75f1732a
josephvutrinh <josephvu.06@gmail.com>
2024-09-14 18:32:57 -0400

    new components

---
+++ components/LocationList.js
@@ -0,0 +1,16 @@
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { useNavigation } from "@react-navigation/native";
+import TopRowPlanRide from "../components/TopRowPlanRide";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const LocationList = () => {
+    return (
+        <View>
+            <Text>Location List</Text>
+        </View>
+    )
+}
+
+export default LocationList;
