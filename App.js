0ef6520317ce4cb2ba1725afc72d99358d5b48e4
johntrinhvu <johntrinhvu@yahoo.com>
2024-09-14 14:39:32 -0400

    make different screens

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import PlanRideScreen from "./screens/PlanRideScreen";
+import ComparePricesScreen from "./screens/ComparePricesScreen";
+import "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name="PlanRideScreen"
+              component={PlanRideScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name="ComparePricesScreen"
+              component={ComparePricesScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/ComparePricesScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const ComparePricesScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-blue-500">I am ComparePricesScreen</Text>
+        </SafeAreaView>
+
+    );
+}
+
+export default ComparePricesScreen;
