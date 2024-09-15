c7243f8f5a3cebd61d3c53229e3f9a8c1c4d3f42
johntrinhvu <johntrinhvu@yahoo.com>
2024-09-15 00:18:25 -0400

    make backend server and fix some pricing

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -5,17 +5,13 @@ import MapView, { Marker } from 'react-native-maps';
 import { selectDestination, selectOrigin, setTravelTimeInformation } from '../slices/navSlice';
 import MapViewDirections from 'react-native-maps-directions';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import darkModeStyle from "../components/darkModeStyle";
 
 const MapComponent = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
   const mapRef = useRef(null);
   const dispatch = useDispatch();
-
-  if (origin && destination) {
-    console.log("Origin: " + origin.description);
-    console.log(destination.description);
-  }
 
   useEffect(() => {
     if (!origin || !destination) return;
@@ -31,9 +27,7 @@ const MapComponent = () => {
     if (!origin || !destination) return;
 
     const getTravelTime = async () => {
-      fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?
-        units=imperial&origins=${origin.description}&destinations=
-        ${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
+      fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
       ).then((res) => res.json())
       .then(data => {
         dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
@@ -47,6 +41,7 @@ const MapComponent = () => {
     <MapView
       ref={mapRef}
       style={{ flex: 1 }}
+      customMapStyle={darkModeStyle}
       initialRegion={{
         latitude: origin?.location?.lat || 37.229572,
         longitude: origin?.location?.lng || -80.413940,
@@ -59,7 +54,7 @@ const MapComponent = () => {
           origin={origin.description}
           destination={destination.description}
           apikey={GOOGLE_MAPS_APIKEY}
-          strokeWidth={3}
+          strokeWidth={6}
           strokeColor="blue"
         />
       )}
